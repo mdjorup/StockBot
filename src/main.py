762b9bf5ad9aab88a5e97cb09518733f919f1fc8
mdjorup <michael.djorup@gmail.com>
2024-01-11 17:48:14 -0500

    put everything into one file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,116 @@
-import boto3
+from dataclasses import dataclass
 
-from tickers import tickers
-from utils import allocate_budget, get_stocks
+import boto3
+import yfinance as yf
 
 budget = 250
+
+
+tickers = [
+    "AAPL",
+    "MSFT",
+    "TSLA",
+    "NVDA",
+    "AMZN",
+    "META",
+    "JPM",
+    "V",
+    "PG",
+    "MA",
+    "HD",
+    "COST",
+    "KO",
+    "PEP",
+    "WMT",
+    "CSCO",
+    "CAT",
+    "GE",
+    "NKE",
+    "LOW",
+    "UBER",
+    "BLK",
+    "SBUX",
+    "CVX",
+    "BX",
+    "PYPL",
+    "TGT",
+    "WM",
+    "CMG",
+    "MMM",
+    "LULU",
+    "COF",
+    "KHC",
+    "DAL",
+    "ABNB",
+    "JNJ",
+]  # Replace with your list of tickers
+
+
+def calculate_rsi(data, window):
+    delta = data.diff()
+    up, down = delta.copy(), delta.copy()
+    up[up < 0] = 0
+    down[down > 0] = 0
+
+    gain = up.rolling(window=window, min_periods=1).mean()
+    loss = down.abs().rolling(window=window, min_periods=1).mean()
+
+    RS = gain / loss
+    RSI = 100 - (100 / (1 + RS))
+
+    return RSI
+
+
+@dataclass
+class Stock:
+    ticker: str
+    rsi: float
+    inverse_rsi: float
+    price: float
+    url: str
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, other):
+        return self.ticker == other.ticker
+
+
+def get_stocks(tickers, period="1mo", rsi_period=14):
+    stocks = []
+
+    for ticker in tickers:
+        data = yf.download(ticker, period=period)
+        if not data.empty:
+            data["RSI"] = calculate_rsi(data["Close"], rsi_period)
+            last_rsi = data["RSI"].iloc[-1]
+
+            new_stock = Stock(
+                ticker,
+                last_rsi,
+                1 / last_rsi,
+                data["Close"].iloc[-1],
+                f"https://finance.yahoo.com/quote/{ticker}",
+            )
+            stocks.append(new_stock)
+        # https://finance.yahoo.com/quote/AAPL
+
+    return stocks
+
+
+def allocate_budget(oversold_stocks, budget):
+    # Calculate the inverse of the RSI for each stock
+
+    # Calculate the total of the inverse RSI values
+    total_inverse_rsi = sum(stock.inverse_rsi for stock in oversold_stocks)
+
+    # Allocate budget based on each stock's share of the total inverse RSI
+    allocations = {}
+    for stock in oversold_stocks:
+        allocation = (stock.inverse_rsi / total_inverse_rsi) * budget
+        allocations[stock] = allocation
+
+    return allocations
 
 
 def send_email_to_sns(subject, message):
